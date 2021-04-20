53eb0166c0afec8a6c893ace5501886a2a219b62
mizuki.yukizane <m.yukizane.4759@outlook.jp>
2021-04-20 15:09:05 +0000

    user login

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+    def gravatar_url(user, options={size: 80})
+        gravatar_id = Digest::MDS::hexgigest(user.email.downcase)
+        size = options[:size]
+        "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    end
 end
